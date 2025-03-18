725f0ee871bffb25827f60881eb8edf227441022
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-03-18 18:30:19 +0100

    database added

---
+++ scripts/database.js
@@ -0,0 +1,99 @@
+let mainCourse = [
+  {
+    "name": "Pizza Margherita",
+    "description": "mit Mozarella",
+    "price": {
+      "regular": 6.9,
+      "large": 7.9,
+    },
+  },
+  {
+    "name": "Pizza Diavola",
+    "description": "mit Salami, Peperoni, Knoblauch und Mozarella",
+    "price": {
+      "regular": 8.9,
+      "large": 9.9,
+    },
+  },
+  {
+    "name": "Pizza Triple P",
+    "description": "mit Champignons, Peperoni, Paprika und Mozarella",
+    "price": {
+      "regular": 7.9,
+      "large": 8.9,
+    },
+  },
+  {
+    "name": "Pizza Quattro Formaggi",
+    "description": "mit vier verschiedenen Käsesorten",
+    "price": {
+      "regular": 8.9,
+      "large": 9.9,
+    },
+  },
+  {
+    "name": "Spaghetti Aglio e Olio",
+    "description": "mit Knoblauch, Olivenöl und Peperoncini",
+    "price": {
+      "regular": 7.5,
+    },
+  },
+  {
+    "name": "Lasagne al Forno",
+    "description": "mit Bolognesesoße und Béchamel",
+    "price": {
+      "regular": 8.5,
+    },
+  },
+];
+
+let sides = [
+  {
+    "name": "Pommes Frites",
+    "description": "frittierte Kartoffelstreifen",
+    "price": {
+      "regular": 2.9,
+      "large": 4.9,
+    },
+  },
+  {
+    "name": "Knoblauchbrot",
+    "description": "geröstetes Brot mit Knoblauchbutter",
+    "price": {
+      "regular": 2.9,
+      "large": 4.9,
+    },
+  },
+  {
+    "name": "Bruschetta",
+    "description": "geröstetes Brot mit Tomaten und Basilikum",
+    "price": {
+      "regular": 3.5,
+      "large": 5.5,
+    },
+  }
+];
+
+let salads = [
+  {
+    "name": "Insalata Mista",
+    "description": "gemischter Salat mit Essig-Öl-Dressing",
+    "price": {
+      "regular": 4.5,
+    },
+  },
+  {
+    "name": "Caesar Salad",
+    "description": "Romanasalat mit Caesar-Dressing, Parmesan und Croutons",
+    "price": {
+      "regular": 5.5,
+    },
+  },
+  {
+    "name": "Caprese",
+    "description": "Tomaten-Mozzarella-Salat mit Basilikum und Olivenöl",
+    "price": {
+      "regular": 5.0,
+    },
+  }
+];
