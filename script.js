a3d4270392ed454bdcd240d59874fbe7438c2cfc
SimonMFuchs <168715756+SimonMFuchs@users.noreply.github.com>
2025-03-19 23:20:23 +0100

    functions for cart added
    pushOrIncreaseAmount(indexIn, arrRef)
    increaseAmount(indexCart, objToCheck)

--- script.js
+++ script.js
@@ -13,17 +13,17 @@ function renderLaCarta(){
     for (let indexLaCarta = 0; indexLaCarta < amountOfArr; indexLaCarta++) {
         let categoryRef = setCategory(arrCount);
         let arrRef = setArr(arrCount);
-        arrCount++;
         laCartaRef.innerHTML += templateLaCarta(indexLaCarta, categoryRef);
-        renderIn(arrRef);
+        renderIn(arrRef, arrCount);      
+        arrCount++;
     }
 }
 
-function renderIn(arrRef){
+function renderIn(arrRef, arrCount){
     let inRef = document.getElementById("la-carta");
 
     for (let indexIn = 0; indexIn < arrRef.length; indexIn++) {
-        inRef.innerHTML += templateContainerIn(indexIn, arrRef);
+        inRef.innerHTML += templateContainerIn(indexIn, arrRef, arrCount);
     }
 }
 
@@ -46,9 +46,42 @@ function setArr(arrCount){
             return sides;
         case 2:
             return salads;
+        default:
+            console.log('!missing array!');
+            break
     }
 }
 
-function addToCart(){
-    
+function addToCart(indexIn, arrCount){
+    let arrRef = setArr(arrCount);
+    pushOrIncreaseAmount(indexIn, arrRef);
+    renderCart(indexIn, arrRef);
+}
+
+function pushOrIncreaseAmount(indexIn, arrRef){
+    let objToCheck = arrRef[indexIn].name;
+    console.log(objToCheck);
+
+    if (cart.find(x => x.name == objToCheck)) {
+        let indexCart = cart.findIndex(dish => dish.name === objToCheck);
+        increaseAmount(indexCart, objToCheck);
+    } else {
+        cart.push(arrRef[indexIn]);
+    }
+}
+
+function increaseAmount(indexCart, obj){
+    let amountRef = document.getElementById(`cart-item${indexCart}-amount`);
+    let amountValueRef = amountRef.innerHTML;
+    amountRef.innerHTML = parseInt(amountValueRef) + 1;
+}
+
+function renderCart(indexIn, arrRef) {
+    let cartContainerRef = document.getElementById('cart');
+    cartContainerRef.innerHTML = "";
+
+    for (let indexCart = 0; indexCart < cart.length; indexCart++) {
+        cartContainerRef.innerHTML += templateCart(indexCart);
+
+    }
 }
--- scripts/templates.js
+++ scripts/templates.js
@@ -1,24 +1,38 @@
 function templateLaCarta(indexLaCarta, categoryRef) {
-    return /*html*/`
+  return /*html*/ `
         <div id="container-out${indexLaCarta}">
             <div class="category-image${indexLaCarta} general-margin-height" alt="Bild der ${categoryRef}"></div>
             <h2>${categoryRef}</h2>
             <div id="container-in${indexLaCarta}"></div>
         </div>
-    `
+    `;
 }
 
-function templateContainerIn(indexIn, arrRef){
-    return /*html*/`
-        <div id="dish-in${indexIn}" class="dish-in-container general-margin-height">
+function templateContainerIn(indexIn, arrRef, arrCount) {
+  return /*html*/ `
+        <div id="dish-in${indexIn}" class="dish-in-container general-margin-height" onclick="addToCart(${indexIn}, ${arrCount})">
             <div class="dish-info-container">            
                 <h3>${arrRef[indexIn].name}</h3>
                 <p>${arrRef[indexIn].description}</p>
                 <p><b>${arrRef[indexIn].price.regular.toFixed(2)}€</b></p>
             </div>
-            <button onclick="addToCart(${indexIn})" class="add-btn">+</button>
+            <button class="add-btn">+</button>
         </div> 
-    `
+    `;
 }
 
-console.log(mainCourse[0].description);
+// console.log(cart[0].name);
+
+function templateCart(indexCart) {
+  return /*html*/ `
+        <div id="cart-item${indexCart}">
+            <h3>${cart[indexCart].name}</h3>
+            <div>
+                <button class="minus-btn" onclick="">-</button>
+                <span  id="cart-item${indexCart}-amount">1</span>
+                <button class="plus-btn" onclick="">+</button>
+                <span id="cart-item${indexCart}-price"> ${cart[indexCart].price.regular} €</span>
+            </div>
+        </div>
+    `;
+}
